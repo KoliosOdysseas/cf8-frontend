43eb64eab340f59f1bee80375133fba505d512f7
kolio <odysseas.kolios.com>
2025-11-05 21:05:40 +0200

    fix: app.html, app.routes, list-group-menu.html, list-group-menu.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,12 @@ import { Step3PersonTableInput } from './components/step3-person-table-input/ste
 import { Step4ForDirective } from './components/step4-for-directive/step4-for-directive';
 import { Step5EventBind } from './components/step5-event-bind/step5-event-bind';
 import { Welcome } from './components/welcome/welcome';
+
 export const routes: Routes = [
   {path:'person-table-example', component: Step2PersonTable},
   {path:'component-input-example', component: Step3PersonTableInput},
   {path:'for-directive-example', component: Step4ForDirective},
-  {path:'event-bind-example' , component:Step5EventBind},
+  {path:"event-bind-example", component:Step5EventBind},
   {path:'welcome', component:Welcome},
   {path:'', redirectTo:'welcome', pathMatch:'full'}
 ];
--- src/app/components/list-group-menu/list-group-menu.ts
+++ src/app/components/list-group-menu/list-group-menu.ts
@@ -3,14 +3,17 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-list-group-menu',
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './list-group-menu.html',
   styleUrl: './list-group-menu.css',
 })
 export class ListGroupMenu {
- menu=[
-  {text:"Step 2 Person Table", link: 'person-table-example'}
-  {text:"Step 3 Component Input" , link:'component-input-exampe'}
-  {text:"Step 4 @for Directive", link:'for-directive-example'}
- ]
+
+  menu = [
+    {text:"Step 2 Person Table", link: 'person-table-example'},
+    {text: "Step 3 Component Input", link: 'component-input-example'},
+    {text: "Step 4 @For Directive", link: 'for-directive-example'},
+    {text: "Step 5 Event Bind", link: 'event-bind-example'}
+  ]
+
 }
