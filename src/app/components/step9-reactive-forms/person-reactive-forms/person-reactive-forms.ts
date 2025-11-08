f573b7e3c5e0e959fccfba4feaadcb5702af1c01
kolio <odysseas.kolios.com>
2025-11-08 11:02:34 +0200

    reactive forms

--- src/app/components/step9-reactive-forms/person-reactive-forms/person-reactive-forms.ts
+++ src/app/components/step9-reactive-forms/person-reactive-forms/person-reactive-forms.ts
@@ -19,4 +19,14 @@ export class PersonReactiveForms {
   lastname: new FormControl('', Validators.required),
   email: new FormControl('', Validators.required)
  })
+ onSubmit(){
+  console.log(this.form.value)
+ }
+ onSetValue(){
+  this.form.setValue({
+    firstname:"lakis",
+    lastname:"lalakis",
+    email:"lakis@aueb.gr"
+  })
+ }
 }
